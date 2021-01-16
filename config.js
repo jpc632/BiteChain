34a0b2b8feb3ac4d03af1afba9cc330960300039
John-Paul Casey <john.paul.casey@gmail.com>
2021-01-16 18:23:22 +1100

    Refactor lib

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const INITIAL_DIFFICULTY = 2;
+const INITIAL_DIFFICULTY = 5;
 const MINE_RATE = 1000;
 
 const GENESIS_DATA = {
-    data : [],
+    data : ["GENESIS"],
     timestamp : 1,
     previousHash: null,
     hash: '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9',
--- index.js
+++ index.js
@@ -1,29 +1,50 @@
 const express = require('express');
-const Blockchain = require('./blockchain');
+const request = require('request');
 const bodyParser = require('body-parser');
-const PubSub = require('./pubsub');
+const { CONFIGURE_PORT } = require('./lib/config');
+const Blockchain = require('./lib/blockchain/index');
+const PubSub = require('./lib/pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
 
-setTimeout(() => pubsub.broadcastChain(), 100);
+const { DEFAULT_PORT, PORT, ROOT_NODE_ADDRESS } = CONFIGURE_PORT(); 
 
+app.listen(PORT, () => {
+    console.log(`The application is listening on port: ${PORT}`);
+
+    if(PORT !== DEFAULT_PORT)
+        syncChains();
+});
+
+/*
+    SYNC BLOCKCHAIN
+*/
+const syncChains = () => {
+    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
+        if(!error && response.statusCode === 200){
+            const rootChain = JSON.parse(body);
+            console.log('replace chain on a sync with ', rootChain);
+            blockchain.replaceChain(rootChain);
+        }
+    });
+};
+
+/*
+    HANDLE REQUESTS
+*/
 app.use(bodyParser.json()); 
 
 app.get('/api/blocks', (req, res) => {
-    // Send response in JSON format
     res.json(blockchain.chain);
 });
 
 app.post('/api/mine', (req, res) => {
     const { data } = req.body;
+
     blockchain.addBlock({ data });
+    pubsub.broadcastChain();
     
     res.redirect('/api/blocks');
 });
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`The application is listening on port: ${PORT}`);
-})
