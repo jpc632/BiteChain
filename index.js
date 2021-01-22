d2018484c98d523dd5be733adc6aa2cf184bbd45
John-Paul Casey <john.paul.casey@gmail.com>
2021-01-22 14:14:59 +1100

    Mine block request

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const Blockchain = require('./lib/blockchain');
 const PubSub = require('./lib/pubsub');
 const TransactionPool = require('./lib/wallet/transaction-pool');
 const Wallet = require('./lib/wallet');
+const TransactionMiner = require('./lib/transaction-miner');
 
 const app = express();
 const blockchain = new Blockchain();
-const transactionPool = new TransactionPool();
 const wallet = new Wallet();
-const pubsub = new PubSub({ blockchain });
+const transactionPool = new TransactionPool();
+const pubsub = new PubSub({ blockchain, transactionPool });
+const transactionMiner = new TransactionMiner({ blockchain, wallet, transactionPool, pubsub });
 
 const { DEFAULT_PORT, PORT, ROOT_NODE_ADDRESS } = CONFIGURE_PORT(); 
 
@@ -19,13 +21,13 @@ app.listen(PORT, () => {
     console.log(`The application is listening on port: ${PORT}`);
 
     if(PORT !== DEFAULT_PORT)
-        syncChains();
+        syncRootState();
 });
 
 /*
-    SYNC BLOCKCHAIN
+    SYNC 
 */
-const syncChains = () => {
+const syncRootState = () => {
     request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
         if(!error && response.statusCode === 200){
             const rootChain = JSON.parse(body);
@@ -33,6 +35,14 @@ const syncChains = () => {
             blockchain.replaceChain(rootChain);
         }
     });
+
+    request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map` }, (error, response, body) => {
+        if(!error && response.statusCode === 200){
+            const rootTransactionPoolMap = JSON.parse(body);
+            console.log('replace transaction pool map on a sync with ', rootTransactionPoolMap);
+            transactionPool.setMap(rootTransactionPoolMap);
+        }
+    })
 };
 
 /*
@@ -46,9 +56,9 @@ app.get('/api/blocks', (req, res) => {
 
 app.post('/api/mine', (req, res) => {
     const { data } = req.body;
-
     blockchain.addBlock({ data });
     pubsub.broadcastChain();
+
     res.redirect('/api/blocks');
 });
 
@@ -64,12 +74,18 @@ app.post('/api/transact', (req, res) => {
     }catch(error){
         return res.status(400).json({ type: 'error', message: error.message});
     }
-    
     transactionPool.setTransaction(transaction);
+    pubsub.broadcastTransaction(transaction);
 
     res.json({ type: 'success', transaction });
 });
 
 app.get('/api/transaction-pool-map', (req, res) => {
-    
+    res.json(transactionPool.transactionMap);
+});
+
+app.get('/api/mine-transactions', (req, res) => {
+    transactionMiner.mineTransactions();
+
+    res.redirect('/api/blocks');
 });
